a405a284aaff502ab866714ed207db8d37646adb
Hamza Nasir <161011944+hamzanasir123@users.noreply.github.com>
2024-03-13 14:28:54 +0500

    first commit

---
+++ Guest_list.js
@@ -0,0 +1,5 @@
+"use strict";
+let Guest_list = ['Kashif', 'Waqas', 'Naveed', 'Azher'];
+for (let i = 0; i < Guest_list.length; i++) {
+    console.log('Dear Mr. ' + Guest_list[i] + '\n\nIts my pleasure to invite you in our party.\n\nThankyou!');
+}
---
+++ Guest_list.ts
@@ -0,0 +1,9 @@
+
+
+let Guest_list :string[] = ['Kashif','Waqas','Naveed','Azher'];
+
+for(let i=0; i<Guest_list.length; i++){
+
+    console.log('Dear Mr. ' + Guest_list[i] + '\n\nIts my pleasure to invite you in our party.\n\nThankyou!') ;
+
+}
